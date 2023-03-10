1b9ab80055b9ac66bf6779886254964b6d7e551c
Anselmo Del hoyo <anselmodelhoyo11@gmail.com>
2023-03-10 17:13:30 -0400

    add: interface basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import './App.css'
+import "./App.css";
+import BasicGrid from "./components/BasicGrid";
+import Navbar from "./components/Navbar";
+import MenuList from "./components/MenuList";
+
+import { Grid } from "@mui/material";
+import Content from "./components/Content";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to my Webapp</h1>
-    </div>
-  )
+	return (
+		<div className="App">
+			<Grid container>
+        <Grid item xs={12}>
+          <Navbar />
+        </Grid>
+        <Grid item xs={3}>
+          <MenuList />
+        </Grid>
+        <Grid item xs={9}>
+          <Content />
+        </Grid>
+      </Grid>
+			{/* <BasicGrid /> */}
+		</div>
+	);
 }
 
-export default App
+export default App;
